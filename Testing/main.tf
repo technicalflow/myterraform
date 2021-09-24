0ce39df08f7e613ab7b9ea400ec53f8bea5b2962
Marek <techfellow@outlook.com>
2021-09-24 11:55:53 +0200

    keyvault added

--- Testing/main.tf
+++ Testing/main.tf
@@ -1,1 +1,20 @@
 # terraform
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+variable "kv_id" {
+  default = "/subscriptions/kv_id"
+}
+
+data "azurerm_key_vault_secret" "kv_secret" {
+  name         = "kvpassword"
+  key_vault_id = var.kv_id
+}
+
+output "KV_Secret_is" {
+  value = data.azurerm_key_vault_secret.kv_secret.value
+}
