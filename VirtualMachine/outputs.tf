76a16e692a5a635d5a46afbe359292c75fb1fb3b
Marek <techfellow@outlook.com>
2021-09-24 12:36:16 +0200

    outputs update

--- VirtualMachine/outputs.tf
+++ VirtualMachine/outputs.tf
@@ -1,6 +1,10 @@
 output "PIP" {
   value = azurerm_public_ip.msapip1.ip_address
 }
+
+output "VM_private_IP" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
 output "DNS" {
   value = azurerm_public_ip.msapip1.domain_name_label
 }
