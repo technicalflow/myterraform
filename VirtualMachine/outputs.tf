6d78522a692e50f3efc818bd4b82df3822be385c
Marek <51706013+technicalflow@users.noreply.github.com>
2021-09-24 15:43:28 +0200

    changes in NSG

--- VirtualMachine/outputs.tf
+++ VirtualMachine/outputs.tf
@@ -1,12 +1,14 @@
 output "PIP" {
-  value = azurerm_public_ip.msapip1.ip_address
+  value = azurerm_public_ip.msapip1.*.ip_address
 }
-
 output "VM_private_IP" {
   value = azurerm_network_interface.nic.private_ip_address
 }
 output "DNS" {
   value = azurerm_public_ip.msapip1.domain_name_label
+}
+output "NSG" {
+  value = azurerm_network_security_group.nsg.name
 }
 
 output "FQDN" {
