4d1558bc7cfc5e83689bfd3c44d3ad109a57e2f8
Marek <51706013+technicalflow@users.noreply.github.com>
2021-11-11 20:20:41 +0100

    small changes

--- Keyvault/kv.tf
+++ Keyvault/kv.tf
@@ -10,6 +10,10 @@ resource "azurerm_key_vault" "kv1" {
   enabled_for_disk_encryption = true
   purge_protection_enabled    = false # so we can fully delete it
 
+  depends_on = [
+      azurerm_resource_group.rg1,
+  ]
+
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
