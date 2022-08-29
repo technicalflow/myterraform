7f86666a90f0780428f96977a5d790b8e38c6d91
Marek <marek@techfellow.it>
2022-08-29 12:03:10 +0200

    added vlan

---
+++ TestIP/vlan/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+}
